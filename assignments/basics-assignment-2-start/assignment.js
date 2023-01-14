e5c5d1d8348fed21c83164ec07e3a6323b01a2c0
Andre Posmitny <40007650+AndrePosm@users.noreply.github.com>
2023-01-14 16:49:30 +0000

    1 task of ass 2 added

--- assignments/basics-assignment-2-start/assignment.js
+++ assignments/basics-assignment-2-start/assignment.js
@@ -1,1 +1,13 @@
 const task3Element = document.getElementById('task-3');
+
+function forTest (){
+    allert ('Hi Andre!');
+}
+allert ('Andre!')
+
+function callName (Name1) {
+    alert ('Hallo' + Name1);
+}
+
+forTest();
+callName('Vasyl');
