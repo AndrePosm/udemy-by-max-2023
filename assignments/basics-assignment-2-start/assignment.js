dd0d7462855343c2c942225d3e401ddca2473b5c
Andre Posmitny <40007650+AndrePosm@users.noreply.github.com>
2023-01-15 13:29:33 +0000

    Add all assign subbmision

--- assignments/basics-assignment-2-start/assignment.js
+++ assignments/basics-assignment-2-start/assignment.js
@@ -5,9 +5,19 @@ function forTest (){
 }
 allert ('Andre!')
 
-function callName (Name1) {
-    alert ('Hallo' + Name1);
+function callName (name1) {
+    alert ('Hallo' + name1);
 }
 
-forTest();
+function combine (str1, str2, str3) {
+    const combineText = '${str1} ${str2} ${str3}';
+    return = combineText;
+
+}
+
 callName('Vasyl');
+
+task3Element.addEventListener('click' greet);
+
+const combinedString = ('Hi', 'to', 'me!');
+alert(combinedString);
