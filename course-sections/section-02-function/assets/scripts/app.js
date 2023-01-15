e9078d1a12cbd442f720200f43bbefc90cf97da0
Andre Posmitny <40007650+AndrePosm@users.noreply.github.com>
2023-01-15 16:25:35 +0000

    Add a re-usable function

--- course-sections/section-02-function/assets/scripts/app.js
+++ course-sections/section-02-function/assets/scripts/app.js
@@ -1,46 +1,53 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 let logEntries = [];
-
-
-
 // also works 
 // let logEntries = [1, 2, 3];
-
-
 //Just one more comment!
-
 
 function getUserNumberInput() {
     return parseInt(usrInput.value);
 }
 
 // Generates and writes calculation log
+
+function writeToLog(
+    operationIdentifier,
+    prevResult,
+    operationNumber,
+    newResult
+){
+    const logEntry = {
+        operation: operationIdentifier,
+        prevResult: prevResult,
+        number: operationNumber,
+        result: newResult
+    };
+    logEntries.push(logEntry);
+    console.log(logEntry.operation);
+    console.log(logEntries);
+}
+
 function createAndWriteOutput (operator, resultBeforeCalc,calcNumber){
     const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
     outputResult(currentResult, calcDescription);// from vendor file
 }
 
-function add (num1, num2) {
+
+function add () {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
     currentResult += enteredNumber;
     createAndWriteOutput ('+', initialResult, enteredNumber);
-    const logEntry = {
-        operation: 'ADD',
-        prevResult: initialResult,
-        number: enteredNumber,
-        result: currentResult
-    };
-    logEntries.push(logEntry);
-    console.log(logEntries);
-}
-
+    writeToLog ('ADD', initialResult, enteredNumber, currentResult);
+    }
+    
 function subtract () {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
     currentResult = currentResult - enteredNumber;
     createAndWriteOutput ('-', initialResult, enteredNumber);
+    writeToLog('SUBSTRACT', initialResult, enteredNumber, currentResult);
 }
 
 function multiply () {
@@ -48,7 +55,7 @@ function multiply () {
     const initialResult = currentResult;
     currentResult = currentResult * enteredNumber;
     createAndWriteOutput("*", initialResult, enteredNumber);
-
+    writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
 }
 
 function divide () {
@@ -56,6 +63,7 @@ function divide () {
     const initialResult = currentResult;
     currentResult = currentResult / enteredNumber;
     createAndWriteOutput("/", initialResult, enteredNumber);
+    writeToLog('DIVIDE', initialResult, enteredNumber, currentResult);
 
 }
 
