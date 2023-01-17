2cb07d1ea227a33c28e53a4423141ef13edcce85
Andre Posmitny <40007650+AndrePosm@users.noreply.github.com>
2023-01-17 14:05:38 +0000

    Add "IF" statement instead in "+" and "-" func

--- course-sections/section-04-if/assets/scripts/app.js
+++ course-sections/section-04-if/assets/scripts/app.js
@@ -33,21 +33,28 @@ function createAndWriteOutput (operator, resultBeforeCalc,calcNumber){
     outputResult(currentResult, calcDescription);// from vendor file
 }
 
-
-function add () {
+function calculatedResult (calculationType) {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult += enteredNumber;
-    createAndWriteOutput ('+', initialResult, enteredNumber);
-    writeToLog ('ADD', initialResult, enteredNumber, currentResult);
+    let mathOperator;
+    if (calculationType === 'ADD') {
+        currentResult += enteredNumber;
+        mathOperator = '+';
+    } else {
+        currentResult -=  enteredNumber;
+        mathOperator = '-';
+    }
+    
+    createAndWriteOutput(mathOperator, initialResult, enteredNumber);
+    writeToLog(calculationType, initialResult, enteredNumber, currentResult);
+}
+
+function add () {
+    calculatedResult ('ADD');
     }
     
 function subtract () {
-    const enteredNumber = getUserNumberInput();
-    const initialResult = currentResult;
-    currentResult = currentResult - enteredNumber;
-    createAndWriteOutput ('-', initialResult, enteredNumber);
-    writeToLog('SUBSTRACT', initialResult, enteredNumber, currentResult);
+    calculatedResult ('SUBTRACT');
 }
 
 function multiply () {
