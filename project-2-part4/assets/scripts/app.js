0be452ad9e189c1be437bdd86677f8ad6ccb0ef7
Andre Posmitny <40007650+AndrePosm@users.noreply.github.com>
2023-01-23 21:41:52 +0000

    Add ann example of for func

--- project-2-part4/assets/scripts/app.js
+++ project-2-part4/assets/scripts/app.js
@@ -225,15 +225,26 @@ function healPlayerHandler () {
 
 function printLogHandler () {
   for (let i = 0; i < 3; i++) {
-    console.log ('---------');
+    console.log("---------");
   }
   /*
   for (let i = 10; i > 0;) {
     i--;
     console.log(i);
   }
+  
+  for (let i = 0; i < battleLog.length; i++) {
+    console.log(battleLog[i]);
+  }
   */
-  console.log(battleLog);
+  let i = 0;
+  for (const logEntry of battleLog) {
+    console.log (`#${i}`);
+    for (const key in logEntry) {
+      console.log(`${key} => ${logEntry[key]}`);
+    }
+    i++;
+  }
 }
 
 attackBtn.addEventListener ('click', attackHandler);
