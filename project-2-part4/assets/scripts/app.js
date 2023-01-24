9066ab41421604732fe078a194cd4704b151ed14
Andre Posmitny <40007650+AndrePosm@users.noreply.github.com>
2023-01-24 13:01:19 +0000

    add dif while loop for consol test

--- project-2-part4/assets/scripts/app.js
+++ project-2-part4/assets/scripts/app.js
@@ -224,22 +224,27 @@ function healPlayerHandler () {
 }
 
 function printLogHandler () {
-  for (let i = 0; i < 3; i++) {
-    console.log("---------");
-  }
-  /*
-  for (let i = 10; i > 0;) {
-    i--;
-    console.log(i);
-  }
+ for (let i = 0; i < 3; i++) {
+   console.log("---------");
+ }
+  let j = 0;
+  do {
+    console.log (j);
+    j++;
+} while (j < 3);
+
+ // for (let i = 10; i > 0;) {
+ //   i--;
+ //   console.log(i);
+ // }
   
-  for (let i = 0; i < battleLog.length; i++) {
-    console.log(battleLog[i]);
-  }
-  */
+//  for (let i = 0; i < battleLog.length; i++) {
+//    console.log(battleLog[i]);
+//  }
+
   let i = 0;
   for (const logEntry of battleLog) {
-    console.log (`#${i}`);
+    console.log(`#${i}`);
     for (const key in logEntry) {
       console.log(`${key} => ${logEntry[key]}`);
     }
